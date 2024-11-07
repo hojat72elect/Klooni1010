9d8c0890876f39a77fe5e1a9eacd567ec336eafa
hojat72elect <hojat72elect@gmail.com>
2024-11-07 14:33:41 -0500

    Cleaned and reformatted.

--- core/src/dev/lonami/klooni/actors/SoftButton.java
+++ core/src/dev/lonami/klooni/actors/SoftButton.java
@@ -6,13 +6,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
 import dev.lonami.klooni.Klooni;
 import dev.lonami.klooni.Theme;
 
-// Small wrapper to use themed image buttons more easily
+/**
+ * A small wrapper to use themed image buttons more easily.
+ */
 public class SoftButton extends ImageButton {
-
 
     private final int styleIndex;
     public Drawable image;
-    //region Constructor
 
     public SoftButton(final int styleIndex, final String imageName) {
         super(Klooni.theme.getStyle(styleIndex));
@@ -20,7 +20,6 @@ public class SoftButton extends ImageButton {
         this.styleIndex = styleIndex;
         updateImage(imageName);
     }
-    //region Public methods
 
     public void updateImage(final String imageName) {
         image = Theme.skin.getDrawable(imageName);
@@ -28,10 +27,13 @@ public class SoftButton extends ImageButton {
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        // Always update the style to make sure we're using the right image.
-        // This might not always be the case since two buttons can be using
-        // the "same" style (except for the image up, i.e. after coming from
-        // the customize menu), so make sure to update it always.
+
+        /*
+         * Always update the style to make sure we're using the right image.
+         * This might not always be the case since two buttons can be using
+         * the "same" style (except for the image up, i.e. after coming from
+         * the customize menu), so make sure to update it always.
+         */
         ImageButtonStyle style = getStyle();
         Klooni.theme.updateStyle(style, styleIndex);
         style.imageUp = image;
@@ -39,6 +41,4 @@ public class SoftButton extends ImageButton {
         getImage().setColor(Klooni.theme.foreground);
         super.draw(batch, parentAlpha);
     }
-
-    
 }
