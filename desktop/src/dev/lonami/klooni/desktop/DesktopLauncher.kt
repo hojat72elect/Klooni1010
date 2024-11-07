8edb7f684d519232a2229bc5c14e73d214b030fc
hojat72elect <hojat72elect@gmail.com>
2024-11-07 16:39:58 -0500

    DesktopLauncher.java -> kt

--- desktop/src/dev/lonami/klooni/desktop/DesktopLauncher.kt
+++ desktop/src/dev/lonami/klooni/desktop/DesktopLauncher.kt
@@ -1,19 +1,18 @@
-package dev.lonami.klooni.desktop;
+package dev.lonami.klooni.desktop
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import dev.lonami.klooni.Klooni;
+import com.badlogic.gdx.Files
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import dev.lonami.klooni.Klooni
 
-class DesktopLauncher {
-    public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Klooni 1010!";
-        config.width = Klooni.GAME_WIDTH;
-        config.height = Klooni.GAME_HEIGHT;
-        config.addIcon("ic_launcher/icon128.png", Files.FileType.Internal);
-        config.addIcon("ic_launcher/icon32.png", Files.FileType.Internal);
-        config.addIcon("ic_launcher/icon16.png", Files.FileType.Internal);
-        new LwjglApplication(new Klooni(null), config);
+fun main(args: Array<String>) {
+    with(LwjglApplicationConfiguration()) {
+        title = "Klooni 1010!"
+        width = Klooni.GAME_WIDTH
+        height = Klooni.GAME_HEIGHT
+        addIcon("ic_launcher/icon128.png", Files.FileType.Internal)
+        addIcon("ic_launcher/icon32.png", Files.FileType.Internal)
+        addIcon("ic_launcher/icon16.png", Files.FileType.Internal)
+        LwjglApplication(Klooni(null), this)
     }
 }
